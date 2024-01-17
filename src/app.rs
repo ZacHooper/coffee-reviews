749fcd76a7ba7f44bdf818f6489eb66ac9a0ffdc
Zac Hooper <zac.g.hooper@gmail.com>
2024-01-18 09:47:08 +1100

    fix: try annnnother tempalte

--- src/app.rs
+++ src/app.rs
@@ -1,37 +1,61 @@
 use leptos::*;
-use leptos_router::*;
 
 #[component]
 pub fn App() -> impl IntoView {
     view! {
-        <Router>
-            <Routes>
-                <Route path="" view=  move || view! { <Home/> }/>
-            </Routes>
-        </Router>
+        <div class="font-fira-mono flex flex-col bg-stone-950 min-h-screen text-base text-center text-gray-300">
+            <div class="flex flex-1 flex-col items-center">
+                <Header/>
+                <Home/>
+                <Footer/>
+            </div>
+        </div>
     }
 }
 
 #[component]
 fn Home() -> impl IntoView {
-    let (count, set_count) = create_signal(0);
-
     view! {
-        <div class="my-0 mx-auto max-w-3xl text-center">
-            <h2 class="p-6 text-4xl">"Welcome to Leptos with Tailwind"</h2>
-            <p class="px-10 pb-10 text-left">"Tailwind will scan your Rust files for Tailwind class names and compile them into a CSS file."</p>
-            <button
-                class="bg-coffee-dark hover:bg-coffee-lightest px-5 py-3 text-white rounded-lg"
-                on:click=move |_| set_count.update(|count| *count += 1)
-            >
-                "Something's here | "
-                {move || if count() == 0 {
-                    "Click me!".to_string()
-                } else {
-                    count().to_string()
-                }}
-                " | Some more text"
-            </button>
+        <div class="pb-20 flex flex-1 items-center mx-4">
+            <div class="mx-auto max-w-3xl text-center md:space-y-20 space-y-14">
+                <div>
+                    <h1 class="pb-6 md:text-2xl text-lg text-rose-400 font-bold">"A Website Written in Rust"</h1>
+                    <p class="m-auto w-3/4 md:text-xl">
+                        "This is a personal CSR Website Template made with "
+                        <a class="text-rose-300 hover:text-rose-200 active:text-rose-200 transition duration-200"
+                        href="https://leptos.dev/" target="_blank">"Leptos"
+                        </a>
+                        ", "
+                        <a class="text-rose-300 hover:text-rose-200 active:text-rose-200 transition duration-200"
+                        href="https://trunkrs.dev/" target="_blank">"Trunk"</a>
+                        " and "
+                        <a class="text-rose-300 hover:text-rose-200 active:text-rose-200 transition duration-200"
+                            href="https://tailwindcss.com/" target="_blank">"Tailwind"</a>
+                    </p>
+                </div>
+                <div class="md:text-base text-sm">
+                    <p>"You can check the source code "
+                        <a class="border-b border-b-rose-300 text-rose-300 hover:text-rose-200 hover:border-b-rose-200 active:text-rose-200 active:border-b-rose-200 transition duration-200"
+                            href="https://github.com/lpnh/leptrun" target="_blank">"here"</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    }
+}
+
+#[component]
+fn Header() -> impl IntoView {
+    view! {
+        <div class="tracking-widest mt-4 flex sm:text-lg text-amber-200 justify-center items-center inset-x-0 top-0 bottom-auto">"Hello, friend!"</div>
+    }
+}
+
+#[component]
+fn Footer() -> impl IntoView {
+    view! {
+        <div class="pb-0 m-auto left-0 right-0">
+            <div class="text-xs">"© 2023 lpnh"</div>
         </div>
     }
 }
